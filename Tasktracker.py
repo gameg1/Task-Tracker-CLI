351f60812ee9daafcc145751eae6b8a0fa63f9ad
Callum Staras <Callum.staras@googlemail.com>
2025-07-15 16:52:10 +0100

    Added task adding functionallity Added the ability to list the task that were added

--- Tasktracker.py
+++ Tasktracker.py
@@ -22,15 +22,17 @@ pars = argparse.ArgumentParser(
     epilog= "Example usage: python Tasktracker.py filename.json -a 'New Task"
 )
 # Creating the arguments
-pars.add_argument("-a","--add", help= "Adds a task to the list",required= False)
-pars.add_argument("-l","--list", help="Type '-l todo/in-progress/done/all to show that cateory of tasks",requierd= False)
-pars.add_argument("-m","--mark",help="Changes the status of desired task to todo/in-progress/done",requierd= False)
-pars.add_argument("-d","--delete",help="Deletes the desired task",requierd= False)
-pars.add_argument("-u","--update",help="Changes the description of the desired task",requierd= False)
 
-#Processing the argument
+pars.add_argument("message", help = "this is the description for a task", nargs= "?", default= "")
+pars.add_argument("-a","--add", help= "Adds a task to the list",type= str)
+pars.add_argument("-l","--list", help="Type '-l todo/in-progress/done/all to show that cateory of tasks",choices= ["todo","in-progress","done","all"], default = "all")
+pars.add_argument("-m","--mark",help="Changes the status of desired task to todo/in-progress/done",type=str, choices =["todo","in-progress","done"])
+pars.add_argument("-d","--delete",help="Deletes the desired task", type = int, default= -1)
+pars.add_argument("-u","--update",help="Changes the description of the desired task via id")
 
-args = pars.parse_args()
+# Processing the argument
+# Allows use to use the arguments and get the information to usterlize it.
+args:argparse.Namespace = pars.parse_args()
 
 file_path = "task_tracker.json"
 
@@ -40,15 +42,26 @@ formatted_time = time.strftime('%H:%M %d %M %Y')
 def add_task():
     pass
 
-def update_task(id):
+def update_task(id:int = -1):
+    print(args.update, args.message)
+def delete_task(id:int = -1):
     pass
-def delete_task(id):
-    pass
-def update_status(id, status):
+def update_status(id:int = -1):
+    print(id, args.mark)
+
+def list_task():
     pass
 
-def list_task ():
+def upadate_task_id():
     pass
 
-def upadate_task_it():
-    pass
+if args.add:
+    add_task()
+if args.list:
+    list_task()
+if args.mark:
+    update_status()
+if args.delete:
+    delete_task()
+if args.update:
+    update_task()
