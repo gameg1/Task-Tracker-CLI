d6e005016ef1054c2d6ad62f25c7f71528ac52c3
Callum Staras <Callum.staras@googlemail.com>
2025-07-22 16:49:31 +0100

    Tidy up the code base Added comments where nessisery

--- Tasktracker.py
+++ Tasktracker.py
@@ -44,51 +44,40 @@ def main():
                     list_task(args.arguments[0])
             elif len(args.arguments) == 0:
                 list_task()
+
+# The fire path for the json database
 file_path = "task_tracker.json"
 
+# Time for timekeeping
 time = datetime.now()
 formatted_time = time.strftime('%H:%M %d %m %Y')
-# Task Properties
-# id: A unique identifier for the task
-# description: A short description of hte task
-# status: the status of the task (todo, in-progress, done)
-# createdAt: The date and time when the task was created
-# updatedAt: The date and time when the task was updated
-def add_task(message:str):
-    # add the add argument into task
-    if os.path.exists(file_path):
-        with open(file_path,"r") as file:
-                data = json.load(file)
 
-                last_id = max(item["id"] for item in data) if data else 0
-                tasks = {
-                "id": last_id + 1,
+def add_task(message:str): # add the add argument into hte json database
+    task = {
+                "id": -1,
                 "description":message,
                 "status": "todo",
                 "createdAt": formatted_time,
                 "updatedAt": formatted_time
                 }
-                data.append(tasks)
+    if os.path.exists(file_path): # If the file exsit in file_path
+        with open(file_path,"r") as file:
+                data = json.load(file)
+                last_id = max(item["id"] for item in data) if data else 0 # Gets the larget id in the json
+                task["id"] = last_id + 1
+                data.append(task)
         with open(file_path,"w") as file:
                 json.dump(data, file, indent = 4)
                 print(f"task added at id: {last_id + 1}")
-    else:
-        tasks = [
-            {
-                "id":1,
-                "description":message,
-                "status": "todo",
-                "createdAt": formatted_time,
-                "updatedAt": formatted_time
-            }
-            ]
+    else: # If the file dosen't exsit in file_path
+        task["id"] = 1
+        tasks = [task]
         with open(file_path,"w") as file:
             json.dump(tasks, file, indent = 4)
             print("task added at id: 1")
 
 
-
-def update_task(id:int = -1, message = ""):
+def update_task(id:int = -1, message = ""): # Changes the message of the task using the task's id
     print(id)
     update = False
     item_to_update = None
@@ -108,7 +97,7 @@ def update_task(id:int = -1, message = ""):
                 json.dump(data, file, indent = 4)
             print(f"task id: {id} has been updated to {message}")
 
-def delete_task(id:int = -1):
+def delete_task(id:int = -1): # deletes the task using the task's id
     removetask = False
     task_to_remove = None
     with open(file_path, "r") as file:
@@ -128,7 +117,7 @@ def delete_task(id:int = -1):
         else:
             pass
 
-def update_status(id:int = -1, status:str = "todo"):
+def update_status(id:int = -1, status:str = "todo"): # updates the task's status using the task's id
     statusupdate = False
     task_to_update = None
     with open(file_path, "r") as file:
@@ -146,20 +135,19 @@ def update_status(id:int = -1, status:str = "todo"):
                 json.dump(data,file,indent=4)
             print(f"file id: {id} status has been updated to {status}")
 
-def list_task(mode:str = ""):
+def list_task(mode:str = "all"): # prints out a list of the tasks with their id's and their status. can be filterd via their status
     with open(file_path, "r") as file:
         data = json.load(file)
         for item in data:
-            if item["status"] == mode or mode == "": # If the task is the same as the status we are looking for or if we are looking for all the tasks
+            if item["status"] == mode or mode == "all": # If the task is the same as the status we are looking for or if we are looking for all the tasks
                 print (f"id:{item["id"]}, desc: {item["description"]}, status: {item["status"]}")
 
 
 
-def upadate_task_id(id:int = -1):
-    task = None
+def upadate_task_id(id:int = -1): # Changes the id of each task so that there isnt any unused id from 1 - n
     with open(file_path,"r") as file:
         data = json.load(file)
-        for index,item in enumerate(data):
+        for item in data:
             if int(item["id"]) > id:
                 data.remove(item)
                 item["id"] = int(item["id"]) - 1
